9abb2f21a9635e0f6b1c6670b826d083ebf38112
Sótanyi Bálint <sotanyibalint01@gmail.com>
2022-12-13 20:46:33 +0100

    Uncommented 1 line

--- config.php
+++ config.php
@@ -27,6 +27,6 @@ if ($_CONFIG['debug']) {
     error_reporting(E_ALL); 
 }
 
-// DB::connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_CHARSET);
+$DB = DB::connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_CHARSET);
 
 // ============================================================================ //
--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 require_once('config.php');
 require_once('functions.php');
 
-$DB = new DB(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_CHARSET);
-
 view('elements', [
     'title' => 'Hellobello',
 ]);
