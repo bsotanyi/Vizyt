6cc145247c09ba3eb35c8c13ee19962c7e57e2ce
Sótanyi Bálint <sotanyibalint01@gmail.com>
2022-12-13 20:44:03 +0100

    Added sample db connection instance

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require_once('config.php');
 require_once('functions.php');
 
+$DB = new DB(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_CHARSET);
+
 view('elements', [
     'title' => 'Hellobello',
 ]);
