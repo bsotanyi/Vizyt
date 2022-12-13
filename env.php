e48651e0013261494de27d4da24af5af5a174c3f
Sótanyi Bálint <sotanyibalint01@gmail.com>
2022-12-13 19:37:06 +0100

    Removed env file

--- env.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// define('DB_HOST', 'localhost');
-// define('DB_NAME', 'frigo_db');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', 'password');
-// define('DB_CHARSET', 'utf8mb4');
