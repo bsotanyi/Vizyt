e2d6b7190e5310f988240c840958c8c3f56a5fd6
Sótanyi Bálint <sotanyibalint01@gmail.com>
2022-12-28 23:37:28 +0100

    Updated sidenav

--- views/components/sidenav.blade.php
+++ views/components/sidenav.blade.php
@@ -4,13 +4,13 @@
             <strong>{{ $title }}</strong>
         </a>
         <div>
-            <a href="/" class="{{ ($active_page ?? '') == 'home' ? 'active' : '' }}">
+            <a href="/" class="@if ($active_page == 'home') active @endif">
                 <i icon-name="home"></i>Home
             </a>
-            <a href="/profile" class="{{ ($active_page ?? '') == 'profile' ? 'active' : '' }}">
+            <a href="/profile" class="@if ($active_page == 'profile') active @endif">
                 <i icon-name="user"></i>Profile
             </a>
-            <a href="/table" class="{{ ($active_page ?? '') == 'table' ? 'active' : '' }}">
+            <a href="/table" class="@if ($active_page == 'table') active @endif">
                 <i icon-name="table"></i>Table
             </a>
         </div>
