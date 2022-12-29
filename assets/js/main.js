eafbf9d40067348e82756f0fe56ecbf49ef3b935
Sótanyi Bálint <sotanyibalint01@gmail.com>
2022-12-29 17:33:36 +0100

    User profile SB

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,7 @@ AOS.init({
 
 window.addEventListener('DOMContentLoaded', function() {
     initPristineValidation();
+    initRepeatables();
     lucide.createIcons();
     qs('.js-sidenav-toggle').onclick = function() {
         document.body.classList.toggle('sidenav-visible');
@@ -31,5 +32,30 @@ function initPristineValidation() {
             });
         })(pristine);
         item.classList.add('js-validate-inited');
+    }
+}
+
+function initRepeatables() {
+    for (let item of qsa('.js-repeatable:not(.js-repeatable-inited)')) {
+        let template_html = qs('script[type="text/template"]', item).innerHTML;
+        let add_button = qs('.js-repeatable-add', item);
+        add_button.addEventListener('click', function() {
+            console.log(item)
+            item.insertAdjacentHTML('beforeend', template_html);
+            initPristineValidation();
+            initRepeatables();
+            lucide.createIcons();
+        });
+        item.classList.add('js-repeatable-inited');
+    }
+    for (let del_item of qsa('.js-repeatable .js-repeatable-remove:not(.js-repeatable-inited)')) {
+        del_item.addEventListener('click', function() {
+            let parent = this.closest('.js-repeatable');
+            let this_row = this.closest('.js-repeatable-row');
+            if (parent && this_row) {
+                parent.removeChild(this_row);
+            }
+        });
+        del_item.classList.add('js-repeatable-inited');
     }
 }
--- views/pages/profile.blade.php
+++ views/pages/profile.blade.php
@@ -5,42 +5,79 @@
 @section('content')
     <div class="parent grid">
         <div>
-            <small>View & Edit</small>
-            <h3>User profile</h3>
-            <hr>
-            <div class="grid-md-fill">
-                <div class="form-group">
-                    <label class="form-label">Username</label>
-                    <input type="text" class="form-control" placeholder="John Doe">
+            <form action="" method="post" class="js-validate">
+                <small>View & Edit</small>
+                <h3>User profile</h3>
+                <hr>
+                <div class="grid-md-fill">
+                    <div class="form-group">
+                        <label class="form-label" for="firstname">First name *</label>
+                        <input type="text" class="form-control" id="firstname" name="firstname" placeholder="John" required>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label" for="lastname">Last name *</label>
+                        <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Doe" required>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label" for="email">E-mail address *</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="user@example.org" required>
+                    </div>
+                    {{-- <div class="form-group">
+                        <label class="form-label">Yes or no</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                            <label class="form-check-label" for="flexCheckDefault">
+                            Default checkbox
+                            </label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label">Message</label>
+                        <textarea class="form-control" cols="30" rows="3" placeholder="Lorem ipsum..."></textarea>
+                    </div> --}}
                 </div>
-                <div class="form-group">
-                    <label class="form-label">Age</label>
-                    <input type="number" class="form-control" value="21">
-                </div>
-                <div class="form-group">
-                    <label class="form-label">Education</label>
-                    <select class="form-select">
-                        <option>High school</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label class="form-label">Yes or no</label>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
-                        <label class="form-check-label" for="flexCheckDefault">
-                        Default checkbox
-                        </label>
+                <div class="grid-2 mt-3">
+                    <div>
+                        <div class="form-group">
+                            <label class="form-label" for="password">Password *</label>
+                            <input type="password" class="form-control" id="password" name="password" minlength="6" required>
+                        </div>
+                        <div class="form-group mt-2">
+                            <label class="form-label" for="password_again">Password again *</label>
+                            <input type="password" class="form-control" id="password_again" name="password_again" required data-pristine-equals="#password">
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="js-repeatable">
+                            <button type="button" class="btn btn-primary float-end js-repeatable-add">
+                                <i icon-name="plus"></i>
+                                Add
+                            </button>
+                            <h3 class="mb-3">Wishlist</h3>
+                            <script type="text/template">
+                                <div class="grid-3 mt-2 js-repeatable-row">
+                                    <input type="text" class="form-control" placeholder="Item name">
+                                    <input type="text" class="form-control" placeholder="Url (optional)">
+                                    <button type="button" class="btn btn-danger square js-repeatable-remove">
+                                        <i icon-name="x"></i>
+                                    </button>
+                                </div>
+                            </script>
+                            @for ($i = 0; $i < 3; $i++)
+                                <div class="grid-3 mt-2 js-repeatable-row">
+                                    <input type="text" class="form-control" placeholder="Item name">
+                                    <input type="text" class="form-control" placeholder="Url (optional)">
+                                    <button type="button" class="btn btn-danger square js-repeatable-remove">
+                                        <i icon-name="x"></i>
+                                    </button>
+                                </div>
+                            @endfor
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="grid mt-3">
-                <div class="form-group">
-                    <label class="form-label">Message</label>
-                    <textarea class="form-control" cols="30" rows="3" placeholder="Lorem ipsum..."></textarea>
-                </div>
-            </div>
-            <br>
-            <button class="btn bg-primary">Submit</button>
+                <br>
+                <button class="btn bg-primary">Submit</button>
+            </form>
         </div>
     </div>
 @endsection
