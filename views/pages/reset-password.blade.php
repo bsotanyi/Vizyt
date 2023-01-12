7bad1fae0faffb24707084b342a0b050e947a1dc
Ádám Tölcsér <tolcserxd@gmail.com>
2023-01-12 18:30:30 +0100

    Reset password page design.

--- views/pages/reset-password.blade.php
+++ views/pages/reset-password.blade.php
@@ -3,14 +3,17 @@
 @section('title', $title)
 
 @section('content')
-    <div class="parent grid">
+    <div class="parent grid-xxl">
         <div>
             <form action="/user/forgot-password" method="post" id="resetPasswordForm">
-                <input type="password" name="password" id="password">
-                <input type="password" name="passwordConfirm" id="passwordConfirm">
-                <input type="hidden" name="email" value="{{ $_GET['email'] }}">
+                <label for="password" class="form-label">Password </label>
+                <input type="password" name="password" id="password" class="form-control">
 
-                <input type="submit" value="Reset Password">
+                <label for="passwordConfirm" class="form-label">Password Confirm </label>
+                <input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control">
+
+                <input type="hidden" name="email" value="{{ $_GET['email'] }}">
+                <input type="submit" value="Reset Password" class="btn btn-primary">
             </form>
         </div>
     </div>
