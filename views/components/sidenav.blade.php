6934f3bbd1e743cc50c9d7e5d98302f78218b0f7
Sótanyi Bálint <sotanyibalint01@gmail.com>
2023-01-15 20:44:43 +0100

    Sidenav updates based on login

--- views/components/sidenav.blade.php
+++ views/components/sidenav.blade.php
@@ -7,18 +7,20 @@
             <a href="/" class="@if ($active_page == 'home') active @endif">
                 <i icon-name="home"></i>Home
             </a>
-            <a href="/user/profile" class="@if ($active_page == 'profile') active @endif">
+            <a href="/user/profile" class="@if ($active_page == 'profile') active @endif" @if(empty($_SESSION['user']))  data-bs-toggle="tooltip" data-bs-title="Please log in to see and edit your profile" onclick="return false" @endif>
                 <i icon-name="user"></i>Profile
             </a>
             <a href="/events" class="@if ($active_page == 'events') active @endif">
                 <i icon-name="calendar-days"></i>Events
             </a>
-            <a href="/admin/dashboard" class="@if ($active_page == 'admin-dashboard') active @endif">
-                <i icon-name="gauge"></i>Dashboard
-            </a>
-            <a href="/admin/users" class="@if ($active_page == 'admin-users') active @endif">
-                <i icon-name="users"></i>Users
-            </a>
+            @if (!empty($_SESSION['user']) && $_SESSION['user']['status'] === 'admin')
+                <a href="/admin/dashboard" class="@if ($active_page == 'admin-dashboard') active @endif">
+                    <i icon-name="gauge"></i>Dashboard
+                </a>
+                <a href="/admin/users" class="@if ($active_page == 'admin-users') active @endif">
+                    <i icon-name="users"></i>Users
+                </a>
+            @endif
         </div>
     </div>
 </aside>
