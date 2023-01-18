a475bbef9537e7b59643a5b4c87851a893091e94
Sótanyi Bálint <sotanyibalint01@gmail.com>
2023-01-18 12:32:13 +0100

    Fixed homepage button label

--- views/pages/home.blade.php
+++ views/pages/home.blade.php
@@ -12,11 +12,11 @@
     <div class="parent grid-xl-fill">
         @if (empty($_SESSION['nearby']))
         <div id="geoloc">
-            <button class="btn bg-primary" onclick="getLocation()">Nearby events are being shown</button>
+            <button class="btn bg-primary" onclick="getLocation()">See nearby events</button>
         </div>
         @else
         <div id="geoloc">
-            <button class="btn bg-primary">See nearby events</button>
+            <button class="btn bg-primary">Nearby events are being shown</button>
         </div>
         @endif
         <div>
